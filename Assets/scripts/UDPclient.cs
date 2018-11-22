11fa5a45215f73de3f25e5c4df4a537c9f796245
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2018-11-22 14:40:41 +0000

    Added custom socket interface

--- Assets/scripts/UDPclient.cs
+++ Assets/scripts/UDPclient.cs
@@ -12,18 +12,64 @@ using System.Net.Sockets;
 
 public class UDPclient : MonoBehaviour {
 
-	// Network variables.
-	private const int MAX_CONNECTION = 2;
-	private int port = 8888;
-
-	private int hostId;
-	private int connectionId;
-	private int channelId;
-	private byte error;
-    
 	ConnectionConfig config;
 	HostTopology topology;
-    
+
+	AsynchronousSocketListener listener = new AsynchronousSocketListener();
+
+    private void Start() {
+				listener.StartListening(8888);
+    }
+
+    //This is the function that serializes the message before sending it
+    void sendMessage(string textInput)
+    {
+
+    }
+
+    private void Update() {
+        //     int outHostId;
+        //     int outConnectionId;
+        //     int outChannelId;
+        //     byte[] buffer = new byte[1024];
+        //     int bufferSize = 1024;
+        //     int receiveSize;
+        //     byte error;
+				//
+        //     NetworkEventType evnt = NetworkTransport.Receive(out outHostId, out outConnectionId, out outChannelId, buffer, bufferSize, out receiveSize, out error);
+        //     switch (evnt)
+        //     {
+        //         case NetworkEventType.ConnectEvent:
+        //             if (outHostId == hostId &&
+        //                 outConnectionId == connectionId &&
+        //                 (NetworkError)error == NetworkError.Ok)
+        //             {
+        //                 Debug.Log("Connected");
+        //             }
+        //             break;
+        //         case NetworkEventType.DisconnectEvent:
+        //             if (outHostId == hostId &&
+        //                 outConnectionId == connectionId)
+        //             {
+        //                 Debug.Log("Connected, error:" + error.ToString());
+        //             }
+        //             break;
+        //     }
+        // }
+				//
+        // // Press the space key to send a test message.
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        //     sendMessage("KILL ALL HUMANS.");
+        // }
+				//
+        // // Press the F1 key to get status on connection.
+        // if (Input.GetKey(KeyCode.F1))
+        // {
+        //     Debug.Log("Current error state:" + error);
+        // }
+    }
+
     // Get local IP address of device.
     public string LocalIPAddress()
     {
@@ -39,89 +85,5 @@ public class UDPclient : MonoBehaviour {
             }
         }
         return localIP;
-    }
-
-    private void Start() {
-        Debug.Log("Initializing Network");
-
-		// Initializing the Transport Layer with no arguments (default settings)
-		NetworkTransport.Init();
-
-		// Initialize connection configuration and add a channel
-		config = new ConnectionConfig();
-		channelId  = config.AddChannel(QosType.Reliable);
-
-		// Maximum of 2 connections
-		topology = new HostTopology(config, MAX_CONNECTION);
-
-		// Get a host ID (I.P. address?)
-		hostId = NetworkTransport.AddHost(topology, port);
-
-        Debug.Log("Host IP: " + LocalIPAddress());
-    } 
-    
-    //This is the function that serializes the message before sending it
-    void sendMessage(string textInput)
-    {
-        byte error;
-        byte[] buffer = new byte[1024];
-        Stream message = new MemoryStream(buffer);
-        BinaryFormatter formatter = new BinaryFormatter();
-        //Serialize the message
-        formatter.Serialize(message, textInput);
-
-        //Send the message from the "client" with the serialized message and the connection information
-        NetworkTransport.Send(hostId, connectionId, channelId, buffer, (int)message.Position, out error);
-
-        //If there is an error, output message error to the console
-        if ((NetworkError)error != NetworkError.Ok)
-        {
-            Debug.Log("Message send error: " + (NetworkError)error);
-        }
-        Debug.Log("From " + hostId + " to " + connectionId + " : " + buffer);
-    }
-
-    private void Update() {
-        {
-            int outHostId;
-            int outConnectionId;
-            int outChannelId;
-            byte[] buffer = new byte[1024];
-            int bufferSize = 1024;
-            int receiveSize;
-            byte error;
-
-            NetworkEventType evnt = NetworkTransport.Receive(out outHostId, out outConnectionId, out outChannelId, buffer, bufferSize, out receiveSize, out error);
-            switch (evnt)
-            {
-                case NetworkEventType.ConnectEvent:
-                    if (outHostId == hostId &&
-                        outConnectionId == connectionId &&
-                        (NetworkError)error == NetworkError.Ok)
-                    {
-                        Debug.Log("Connected");
-                    }
-                    break;
-                case NetworkEventType.DisconnectEvent:
-                    if (outHostId == hostId &&
-                        outConnectionId == connectionId)
-                    {
-                        Debug.Log("Connected, error:" + error.ToString());
-                    }
-                    break;
-            }
-        }
-
-        // Press the space key to send a test message.
-        if (Input.GetKey(KeyCode.Space))
-        {
-            sendMessage("KILL ALL HUMANS.");
-        }
-
-        // Press the F1 key to get status on connection.
-        if (Input.GetKey(KeyCode.F1))
-        {
-            Debug.Log("Current error state:" + error);
-        }
     }
 }
